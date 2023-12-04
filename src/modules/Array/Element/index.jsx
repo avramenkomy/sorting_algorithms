2a041c5472b237d4ba45727068094e9b752da066
avramenkomy <avramenkomy@yandex.ru>
2023-12-04 21:22:12 +0300

    WIP: add auxiliary function iterating on array with colored element and items position exchange

--- src/modules/Array/Element/index.jsx
+++ src/modules/Array/Element/index.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './style.css'
 
-function Element({h, w, id}) {
+function Element({h, w, id, color}) {
 
   const arrElemStyle = {
     height: `calc(2px + ${ h }px)`,
     width: `${ w }px`,
+    backgroundColor: `${color}`,
   }
 
   return (
--- src/modules/Array/index.jsx
+++ src/modules/Array/index.jsx
@@ -2,27 +2,68 @@ import React from 'react';
 import Element from './Element';
 import './style.css';
 
-const array = [
-  10, 100, 61, 12, 34, 57, 89, 44, 95, 11, 29, 10, 100, 61, 12,
-  14, 102, 66, 16, 31, 52, 85, 46, 91, 15, 28, 11, 10, 1, 16,
+let array = [
   10, 100, 61, 12, 34, 57, 89, 44, 95, 11, 29, 10, 100, 61, 12,
 ];
 
-function Array() {
-  let elemWidth = window.screen.width / array.length;  
+const ACTIVE_COLOR = 'white';
+const DEFAULT_COLOR = 'rgb(252, 57, 7)';
+
+function ArrayComponent() {
+  let elemWidth = window.screen.width / array.length;
+
+  const [current, setCurrent] = React.useState(-1);
+  const [arrValue, setArrayValue] = React.useState(array);
+
+  let init = -1;
+  let timeout;
+
+
+
+  const iteratingOnArray = () => {
+    setCurrent(prevState => prevState + 1);
+    init = init + 1;
+
+    timeout = setTimeout(() => {
+      if (init < array.length) {
+        iteratingOnArray();
+      } else {
+        setCurrent(-1);
+        init = -1;
+      }
+    }, 500);
+  }
+
+  const positionExchange = (array, elemPos1, elemPos2) => {
+    if (Array.isArray(array) && elemPos1 < array.length && elemPos2 < array.length) {
+      let arr = Array.from(array);
+      [arr[elemPos1], arr[elemPos2]] = [arr[elemPos2], arr[elemPos1]];
+      return arr;
+    }
+    return;
+  }
+
+  const arrModify = () => {
+    setArrayValue(prevState => positionExchange(prevState, 2, 3));
+  }
 
   return (
-    <div className="array">
-      {array.map((h, idx) => (
-        <Element
-          key={idx}
-          h={h}
-          w={elemWidth}
-          id={idx}
-        />
-      ))}
-    </div>
+    <React.Fragment>
+      <div className="array">
+        {arrValue.map((h, idx) => (
+          <Element
+            key={idx}
+            h={h}
+            w={elemWidth}
+            id={idx}
+            color={idx === current ? ACTIVE_COLOR : DEFAULT_COLOR}
+          />
+        ))}
+      </div>
+      <button onClick={() => iteratingOnArray()}>Start</button>
+      <button onClick={arrModify}>ChangePos</button>
+    </React.Fragment>
   )
 }
 
-export default Array;
+export default ArrayComponent;
