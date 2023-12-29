f1f97151c1eb184446309cd9ff313f37505bb56d
avramenkomy <avramenkomy@yandex.ru>
2023-12-29 19:27:56 +0300

    WIP: create an empty layout

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
+    <React.Fragment>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </React.Fragment>
   );
 }
 
