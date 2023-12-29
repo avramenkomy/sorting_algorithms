5722f6cc5724c1b278c7811b4486bfb68da86b25
avramenkomy <avramenkomy@yandex.ru>
2023-12-29 21:26:10 +0300

    WIP: create store and rootReducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { rootReducer } from './redux/reducers';
+
 import App from './app';
 import './index.css'
+
+const store = createStore(rootReducer, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+// import themeReducer from './themeReducer';
+
+
+const rootReducer = combineReducers({
+  // theme: themeReducer,
+  // ... all reducers connect here
+});
+
+export default rootReducer;
