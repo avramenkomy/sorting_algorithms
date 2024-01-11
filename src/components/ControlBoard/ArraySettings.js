4a736675e2293ef4e8ff8db783fb5fcf6f042745
avramenkomy <avramenkomy@yandex.ru>
2024-01-11 18:04:42 +0300

    WIP: array settings component layout

--- src/components/ControlBoard/ArraySettings.js
+++ src/components/ControlBoard/ArraySettings.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { Paper, Box } from '@mui/material';
+import { Grid, Box, Paper, Button } from '@mui/material';
+
+import RangeElement from '../Elements/Range';
 
 
 function ArraySettings() {
@@ -17,7 +19,35 @@ function ArraySettings() {
         },
       }}
     >
-      <Paper elevation={3} />
+      <Paper elevation={3} sx={{ padding: '4px' }}>
+        <Grid
+          container
+          spacing={1}
+          flexDirection="column"
+          flexWrap="nowrap"
+        >
+          <Grid
+            item
+            container
+            flexGrow={1}
+            flexDirection="column"
+            flexWrap="nowrap"
+            spacing={1}
+          >
+            <Grid item>
+              <RangeElement defaultValue={30} label="Array size" />
+            </Grid>
+
+            <Grid item>
+              <RangeElement defaultValue={30} label="Speed sorting" />
+            </Grid>
+          </Grid>
+
+          <Grid item alignSelf="center">
+            <Button variant="outlined">Create</Button>
+          </Grid>
+        </Grid>
+      </Paper>
     </Box>
   )
 }
